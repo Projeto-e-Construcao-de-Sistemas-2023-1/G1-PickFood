37eb813a5aaa4b44c83b1cfdb9ca3f564b506340
Juan Gomes <juangsantanna@gmail.com>
2023-05-07 23:44:00 -0300

    template do backend inserido no projeto

---
+++ aplicacao/backend/pickfood/src/main/java/br/pickfood/PickfoodApplication.java
@@ -0,0 +1,13 @@
+package br.pickfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickfoodApplication.class, args);
+	}
+
+}
---
+++ aplicacao/backend/pickfood/src/test/java/br/pickfood/PickfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pickfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
