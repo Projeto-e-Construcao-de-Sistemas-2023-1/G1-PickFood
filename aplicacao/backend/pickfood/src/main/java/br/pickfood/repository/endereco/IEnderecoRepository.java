62cdda6a56da9e74e0cfca5e994809c96aecb0e1
Maria Jordana <jordana-cavalcante@outlook.com>
2023-05-22 09:21:15 -0300

    corrigindo extends base de endereco(service e repository)

--- aplicacao/backend/pickfood/src/main/java/br/pickfood/repository/endereco/IEnderecoRepository.java
+++ aplicacao/backend/pickfood/src/main/java/br/pickfood/repository/endereco/IEnderecoRepository.java
@@ -1,10 +1,10 @@
 package br.pickfood.repository.endereco;
 
 import br.pickfood.model.endereco.Endereco;
-import org.springframework.data.jpa.repository.JpaRepository;
+import br.pickfood.repository.IBaseRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+public interface IEnderecoRepository extends IBaseRepository<Endereco> {
 
 }
---
+++ aplicacao/backend/pickfood/src/main/java/br/pickfood/service/endereco/IEnderecoService.java
@@ -0,0 +1,8 @@
+package br.pickfood.service.endereco;
+
+import br.pickfood.model.dto.endereco.EnderecoDTO;
+import br.pickfood.model.endereco.Endereco;
+import br.pickfood.service.IBaseService;
+
+public interface IEnderecoService extends IBaseService<EnderecoDTO,Endereco> {
+}
