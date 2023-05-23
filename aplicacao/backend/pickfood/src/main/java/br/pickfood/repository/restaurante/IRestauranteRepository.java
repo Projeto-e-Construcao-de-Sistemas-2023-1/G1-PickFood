3c8887e168f7c55cc7698c8a0ce31ada5e0dee58
Maria Jordana <jordana-cavalcante@outlook.com>
2023-05-23 15:22:32 -0300

    criando restaurante repository

---
+++ aplicacao/backend/pickfood/src/main/java/br/pickfood/repository/restaurante/IRestauranteRepository.java
@@ -0,0 +1,7 @@
+package br.pickfood.repository.restaurante;
+
+import br.pickfood.model.restaurante.Restaurante;
+import br.pickfood.repository.IBaseRepository;
+
+public interface IRestauranteRepository extends IBaseRepository<Restaurante> {
+}
