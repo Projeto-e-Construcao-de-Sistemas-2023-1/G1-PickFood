a5ceca103d2ad33714f9f245b4dbd324212afeb0
Maria Jordana <jordana-cavalcante@outlook.com>
2023-05-23 15:24:17 -0300

    add anotação column e horarios

--- aplicacao/backend/pickfood/src/main/java/br/pickfood/model/restaurante/Restaurante.java
+++ aplicacao/backend/pickfood/src/main/java/br/pickfood/model/restaurante/Restaurante.java
@@ -16,10 +16,23 @@ import lombok.Setter;
 @Table(name = "Restaurante")
 public class Restaurante extends BaseEntity {
 
+    @Column(name = "nome_fantasia")
     public String nome_fantasia;
+
+    @Column(name = "cnpj")
     public String cnpj;
+
+    @Column(name = "razao_social")
     public String razao_social;
-    public String horario_funcionamento;
+
+    //rever se o tipo do dado ta correto
+    @Column(name = "horario_abertura")
+    public String horario_abertura;
+
+    @Column(name = "horario_fechamento")
+    public String horario_fechamento;
+
+    @Column(name = "taxa_entrega")
     public float taxa_entrega;
 
 
