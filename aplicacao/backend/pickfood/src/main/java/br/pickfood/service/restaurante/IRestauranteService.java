15e9b8ff6bafbe1e4105c883fc9b30170e5fd1a7
Maria Jordana <jordana-cavalcante@outlook.com>
2023-05-23 15:22:51 -0300

    criando restaurante service

---
+++ aplicacao/backend/pickfood/src/main/java/br/pickfood/service/restaurante/IRestauranteService.java
@@ -0,0 +1,8 @@
+package br.pickfood.service.restaurante;
+
+import br.pickfood.model.dto.restaurante.RestauranteDTO;
+import br.pickfood.model.restaurante.Restaurante;
+import br.pickfood.service.IBaseService;
+
+public interface IRestauranteService extends IBaseService<RestauranteDTO, Restaurante> {
+}
---
+++ aplicacao/backend/pickfood/src/main/java/br/pickfood/service/restaurante/RestauranteService.java
@@ -0,0 +1,9 @@
+package br.pickfood.service.restaurante;
+
+import br.pickfood.model.dto.restaurante.RestauranteDTO;
+import br.pickfood.model.restaurante.Restaurante;
+import br.pickfood.repository.restaurante.IRestauranteRepository;
+import br.pickfood.service.BaseServiceImpl;
+
+public class RestauranteService extends BaseServiceImpl<RestauranteDTO, Restaurante, IRestauranteRepository> {
+}
