5f173c3bc8f6bf79b3cb46013eff2b621e3e6f68
Romumoon <romu.pastore@gmail.com>
2023-05-24 23:49:16 -0300

    Ajuste nos favoritos e cliente

--- aplicacao/backend/pickfood/src/main/java/br/pickfood/service/favoritos/FavoritosService.java
+++ aplicacao/backend/pickfood/src/main/java/br/pickfood/service/favoritos/FavoritosService.java
@@ -1,14 +1,29 @@
 package br.pickfood.service.favoritos;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
 import br.pickfood.model.dto.favoritos.FavoritosDTO;
+import br.pickfood.model.dto.restaurante.RestauranteDTO;
 import br.pickfood.model.favoritos.Favoritos;
+import br.pickfood.model.restaurante.Restaurante;
 import br.pickfood.repository.favoritos.IFavoritosRepository;
 import br.pickfood.service.BaseServiceImpl;
+import jakarta.transaction.Transactional;
 
 @Service
 public class FavoritosService extends BaseServiceImpl<FavoritosDTO, Favoritos, IFavoritosRepository>
 	implements IFavoritosService{
-
+	
+	public List<RestauranteDTO> listRestaurantesFavoritosByClient(Integer clientId){
+		List<Restaurante> favoritosList = baseRepository.findByCliente_Id(clientId).stream().map(Favoritos::getRestaurante).collect(Collectors.toList());
+		return favoritosList.stream().map(Restaurante::convertToDto).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public void deleteByClienteIdAndRestauranteId(FavoritosDTO dto) {
+		baseRepository.deleteByCliente_IdAndRestaurante_Id(dto.getClienteId(), dto.getRestauranteId());
+	}
 }
