c79e8082203afce09c46cbf735083c1d9ef9794b
Romumoon <romu.pastore@gmail.com>
2023-05-25 23:39:11 -0300

    Login create

--- aplicacao/backend/pickfood/src/main/java/br/pickfood/model/dto/user/UserDTO.java
+++ aplicacao/backend/pickfood/src/main/java/br/pickfood/model/dto/user/UserDTO.java
@@ -3,17 +3,21 @@ package br.pickfood.model.dto.user;
 import br.pickfood.model.dto.BaseDTO;
 import br.pickfood.model.user.User;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDTO extends BaseDTO<User>{
+	
+	private Integer id;
 	
 	private String email;
 	
 	private String senha;
+	
+	private String type;
 	
 	@Override
 	public User convertToEntity() {
