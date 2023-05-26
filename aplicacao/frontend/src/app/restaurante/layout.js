eda392db8d477ecab5b56a309dfccb625fa96c85
jaum1234 <joaovitorsouzacoura@gmail.com>
2023-05-26 01:56:15 -0300

    commit

--- aplicacao/frontend/src/app/restaurante/layout.js
+++ aplicacao/frontend/src/app/restaurante/layout.js
@@ -1,7 +1,26 @@
+'use client';
+
 import NavbarSuperiorContainer from "@/containers/NavbarSuperiorRestaurante";
 import NavbarInferiorCliente from "@/containers/NavbarInferiorRestaurante";
+import { useRouter } from "next/navigation";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../layout";
 
 export default function RestauranteLayout({ children }) {
+
+    const { usuario } = useContext(AuthContext);
+    const router = useRouter();
+
+    useEffect(() => {
+
+        console.log(usuario)
+        
+        if (!usuario || Object.keys(usuario).length === 0) {
+            router.push("/auth/login");
+        }
+
+    }, [usuario, router])
+
     return(
         <>
             <NavbarSuperiorContainer/>
