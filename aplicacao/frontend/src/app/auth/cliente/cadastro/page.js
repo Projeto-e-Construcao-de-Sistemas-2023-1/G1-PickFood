aef8b89e030b6cef73fe28e771b04c60a9c342b0
Arthur André <arthurdasilvaandre@gmail.com>
2023-05-31 14:30:18 -0300

    Ajeitação de nome da pasta
    Alterei o nome da pasta "form" para "Form" e assim o IDE atualizou o nome das importações

--- aplicacao/frontend/src/app/auth/cliente/cadastro/page.js
+++ aplicacao/frontend/src/app/auth/cliente/cadastro/page.js
@@ -3,7 +3,7 @@
 import Logo from "@/components/Logo";
 import { logo, arrow } from "./styles.module.scss";
 import Image from "next/image";
-import Form from "@/components/form"; 
+import Form from "@/components/Form"; 
 import { useRouter } from "next/navigation";
 import { useContext, useState } from "react";
 import { AuthContext } from "@/app/layout";
--- aplicacao/frontend/src/app/auth/restaurante/cadastro1/page.js
+++ aplicacao/frontend/src/app/auth/restaurante/cadastro1/page.js
@@ -1,6 +1,6 @@
 'use client';
 
-import Form from "@/components/form";
+import Form from "@/components/Form";
 import Container from "@/components/Container";
 import Image from "next/image";
 import Link from "next/link";
