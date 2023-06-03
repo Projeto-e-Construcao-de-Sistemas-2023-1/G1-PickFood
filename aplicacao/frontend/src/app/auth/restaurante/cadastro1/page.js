05c122cebe01e1400b39afbc9dc210427f55e9bc
bruna <brunaruback07@gmail.com>
2023-06-03 15:46:46 -0300

    UseForm cadastro 1 - restaurante

--- aplicacao/frontend/src/app/auth/restaurante/cadastro1/page.js
+++ aplicacao/frontend/src/app/auth/restaurante/cadastro1/page.js
@@ -1,16 +1,17 @@
 'use client';
 
-import Form from "@/components/Form";
 import Container from "@/components/Container";
 import Image from "next/image";
 import Link from "next/link";
 import Logo from "@/components/Logo";
+import Form from "@/components/Form";
+import { useForm } from "react-hook-form";
 import { 
     title,
     center,
     forms,
     arrow,
-    logo
+    logo,
 } from "./styles.module.scss"
 import { useContext, useState } from "react";
 import { CadastroRestauranteContext } from "../layout";
@@ -18,56 +19,36 @@ import { useRouter } from "next/navigation";
 
 export default function Cadastro1() {
 
+    const { register, handleSubmit: submit, formState: { errors } } = useForm();
+
     const { definirDados } = useContext(CadastroRestauranteContext);
 
     const router = useRouter();
 
-    const [email, setEmail] = useState("");
-    const [senha, setSenha] = useState("");
-    const [nomeFantasia, setNomeFantasia] = useState("");
-    const [razaoSocial, setRazaoSocial] = useState("");
-    const [cnpj, setCnpj] = useState("");
+    const handleSubmit = (data) => {
 
-    const handleEmail = (e) => {
+        const { email, senha, razao_social, nome_fantasia, cnpj} = data;
 
-        setEmail(e.target.value);
-    }
-
-    const handleSenha = (e) => {
-
-        setSenha(e.target.value);
-    } 
-
-    const handleNomeFantasia = (e) => {
-
-        setNomeFantasia(e.target.value);
-    }
-
-    const handleRazaoSocial = (e) => {
-
-        setRazaoSocial(e.target.value);
-    }
-
-    const handleCnpj = (e) => {
-
-        setCnpj(e.target.value)
-    }
-
-    const cadastrar = () => {
-
-        definirDados({
+        // request.post("restaurantes", {
+        //      email,
+        //      senha,
+        //      razao_Social,
+        //      nome_Fantasia,
+        //      cnpj
+        //  })
+    
+       definirDados({
             email,
             senha,
-            razaoSocial,
-            nomeFantasia,
+            razao_social,
+            nome_fantasia,
             cnpj
-        });
-
-        router.push("/auth/restaurante/cadastro2");
+         })
     }
 
     return (
         <Container>
+
             <Link href={""}>
             <Image
                 src="/icons/back.svg"
@@ -79,43 +60,41 @@ export default function Cadastro1() {
             </Link>
 
             <Logo className={ logo }/>
-                 
             
             <h2 className={ [center, title].join(' ') }>Informações do restaurante</h2>
+            { Object.keys(errors).length !== 0 && <div style={{ color: "red" }}> Dados inválidos </div> }
+            
+            <div className={forms}>         
+                <Form onSubmit={ handleSubmit }>
+                    <Form.Field>
+                        <Form.Label>Email</Form.Label>
+                        <Form.Input type="email" { ...register("email", { required: true }) }/>
+                    </Form.Field>
+                    
+                    <Form.Field>
+                        <Form.Label>Senha</Form.Label>
+                        <Form.Input type="senha" { ...register("senha", { required: true }) }/>
+                    </Form.Field>
 
-            <div className={forms}>
-            <Form>
-                <Form.Field>
-                    <Form.Label>Email</Form.Label>
-                    <Form.Input value={ email } onChange={ handleEmail }/>
-                </Form.Field>
-                
-                <Form.Field>
-                    <Form.Label>Senha</Form.Label>
-                    <Form.Input value={ senha } type={ "password" } onChange={ handleSenha }/>
-                </Form.Field>
+                    <Form.Field>
+                        <Form.Label>Nome Fantasia</Form.Label>
+                        <Form.Input type="nome_fantasia" { ...register("nome_fantasia", { required: true }) }/>
+                    </Form.Field>
+                    
+                    <Form.Field>
+                        <Form.Label>Razão Social</Form.Label>
+                        <Form.Input type="razao_social" { ...register("razao_social", { required: true }) }/>
+                    </Form.Field>
 
-                <Form.Field>
-                    <Form.Label>Nome Fantasia</Form.Label>
-                    <Form.Input value={ nomeFantasia } onChange={ handleNomeFantasia }/>
-                </Form.Field>
-                
-                <Form.Field>
-                    <Form.Label>Razão Social</Form.Label>
-                    <Form.Input value={ razaoSocial} onChange={ handleRazaoSocial}/>
-                </Form.Field>
+                    <Form.Field>
+                        <Form.Label>CNPJ</Form.Label>
+                        <Form.Input type="cnpj" { ...register("cnpj", { required: true }) }/>
+                    </Form.Field>
 
-                <Form.Field>
-                    <Form.Label>CNPJ</Form.Label>
-                    <Form.Input value={ cnpj } onChange={ handleCnpj }/>
-                </Form.Field>
-
-                <Form.Button onClick={ cadastrar }>Continuar</Form.Button> 
-                
-                
-            </Form>
+                    <Form.Button type= "submit">Continuar</Form.Button> 
+                </Form>
             </div>
-
+        
         </Container>
     )
 }
--- aplicacao/frontend/src/components/form/index.js
+++ aplicacao/frontend/src/components/form/index.js
@@ -1,19 +1,19 @@
 import styles from "./styles.module.scss"
 import Button from "../Button"
-export default function Form({ children }) {
-    return <form className={ styles.form }>{ children }</form>
+export default function Form({ children, ...restProps }) {
+    return <form className={ styles.form } { ...restProps }>{ children }</form>
 }
 
-Form.Field = function FormField({ children }) {
-    return <div className={ styles.field }>{ children }</div>
+Form.Field = function FormField({ children, ...restProps }) {
+    return <div className={ styles.field } { ...restProps }>{ children }</div>
 }
 
-Form.Label = function InputLabel({ children }) {
-    return <label className={ styles.label }>{ children }</label>
+Form.Label = function InputLabel({ children,...restProps}) {
+    return <label className={ styles.label } { ...restProps }>{ children }</label>
 }
 
-Form.Input = function FormInput({ type, value, onChange}) {
-    return <input className={ styles.input } value={ value } onChange={ onChange } type={ type }/>
+Form.Input = function FormInput({ type, value, onChange, ...restProps }) {
+    return <input className={ styles.input } value={ value } onChange={ onChange } type={ type } { ...restProps }/>
 } 
 
 Form.Button = function FormButton({ children, ...restProps }) {
