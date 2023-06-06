91a020ce0be17a0633b7663671de0698e3847fad
jaum1234 <joaovitorsouzacoura@gmail.com>
2023-06-05 22:43:21 -0300

    :sparkles: feat: adicionando campo para inserir imagem.

--- aplicacao/frontend/src/app/(global)/restaurante/criar-prato/page.js
+++ aplicacao/frontend/src/app/(global)/restaurante/criar-prato/page.js
@@ -9,46 +9,68 @@ import {
 } from "./styles.module.scss"
 import Link from "next/link";
 import { useForm } from "react-hook-form";
+import { useEffect } from "react";
 
 export default function CriarPrato() {
 
-    const { register: registrar, handleSubmit: tratarFormulario } = useForm();
+    const { register: registrar, handleSubmit: tratarFormulario, watch } = useForm();
+    const imagemValida = /^https:\/\/[\w.-]+\/.*\.(jpg|png|jpeg)$/i
+
+    useEffect(() => {
+        console.log(watch("imagem"));
+    })
+
+    const criar = (data) => {
+        console.log(data);
+    }
 
     return (
         <Container>
-            <Image
-                src="/icons/foto.svg"
-                width={ 21 }
-                height={ 21 }
-                className={ imagem }
-                alt="Icone de seta apontando para trás."
-            />
+           
 
             <div className={forms}>
-                <Form>
+                <Form onSubmit={ tratarFormulario(criar) }>
+
+                    <Image
+                        src={ 
+                            imagemValida.test(watch("imagem")) ? 
+                                watch("imagem") 
+                                : 
+                                "/icons/foto.svg" 
+                        }
+                        width={ 21 }
+                        height={ 21 }
+                        className={ imagem }
+                        
+                        alt="."
+                    />
+                    
                     <Form.Field>
                         <Form.Label>Nome</Form.Label>
-                        <Form.Input/>
+                        <Form.Input registrar={{ ...registrar("nome") }}/>
                     </Form.Field>
                     
                     <Form.Field>
                         <Form.Label>Tipo</Form.Label>
-                        <Form.Input/>
+                        <Form.Input registrar={{ ...registrar("tipo") }}/>
                     </Form.Field>
 
                     <Form.Field>
                         <Form.Label>Preço</Form.Label>
-                        <Form.Input/>
+                        <Form.Input registrar={{ ...registrar("preco") }}/>
                     </Form.Field>
                     
                     <Form.Field>
                         <Form.Label>Descrição</Form.Label>
-                        <Form.Input/>
+                        <Form.Input registrar={{ ...registrar("descricao") }}/>
                     </Form.Field>
 
-                    <Link href={"/restaurante/cardapio"}>
-                        <Form.Button onClick={ () => authenticate() }>Continuar</Form.Button>
-                    </Link>
+                    <Form.Field>
+                        <Form.Label>Link da imagem</Form.Label>
+                        <Form.Input registrar={{ ...registrar("imagem") }} type={ "text" }></Form.Input>
+                    </Form.Field>
+
+                    <Form.Button>Continuar</Form.Button>
                 </Form>
             </div>
         </Container>
