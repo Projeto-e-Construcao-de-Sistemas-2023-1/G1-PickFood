8dd72316f8b6ed818b7f31017abbf42518a1ccf7
jaum1234 <joaovitorsouzacoura@gmail.com>
2023-06-11 18:37:01 -0300

    :sparkles: feat: adicionando itens no carrinho.

--- aplicacao/frontend/src/app/(global)/cliente/restaurante/[idRestaurante]/prato/[idPrato]/page.js
+++ aplicacao/frontend/src/app/(global)/cliente/restaurante/[idRestaurante]/prato/[idPrato]/page.js
@@ -18,6 +18,7 @@ import prato from "@/fixtures/prato";
 import restaurante from "@/fixtures/restaurante";
 import { useEffect } from "react";
 import Button from "@/components/Button";
+import { v4 as uuid } from "uuid";
 
 const Prato = ({ params }) => {
 
@@ -25,7 +26,53 @@ const Prato = ({ params }) => {
 
     const addCarrinho = () => {
 
-        router.push("/cliente/meu-carrinho");
+        let carrinho = JSON.parse(localStorage.getItem("carrinho"));
+
+        const itemCarrinho = {
+            id: uuid(),
+            quantidade: 1,
+            valor: prato.preco,
+            prato: {
+                ...prato
+            }
+        }
+
+        if (carrinho === null || carrinho.length === 0) {
+        
+            localStorage.setItem("carrinho", JSON.stringify([itemCarrinho]));
+
+        } else {
+
+            let itemEncontrado = false;
+
+            for (const indice in carrinho) {
+                
+                if (carrinho[indice].prato.id === prato.id) {
+                    carrinho[indice].quantidade += 1;
+                    carrinho[indice].valor += prato.preco;
+
+                    console.log(carrinho[indice])
+
+                    
+                    carrinho.push(carrinho[indice]);
+
+                    carrinho.splice(indice, 1);
+
+                    itemEncontrado = true;
+
+                    break;
+                }
+            }
+
+            if (!itemEncontrado) {
+                carrinho.push(itemCarrinho);
+            }
+
+            localStorage.setItem("carrinho", JSON.stringify(carrinho));
+            
+        }
+
+        //router.push("/cliente/meu-carrinho");
     }
 
     return(
