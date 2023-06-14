043d360721702c0deef1f7120ed19c06a5871242
jaum1234 <joaovitorsouzacoura@gmail.com>
2023-06-13 22:56:58 -0300

    :sparkles: feat: adicionando botao para limpar carrinho e corrindo atualizacao do estado.

--- aplicacao/frontend/src/components/Carrinho/index.js
+++ aplicacao/frontend/src/components/Carrinho/index.js
@@ -7,6 +7,7 @@ import {
     nomeRestaurante,
     adicionarMaisItens,
     pratos,
+    limpar,
     prato,
     info,
     nomePrato,
@@ -19,6 +20,7 @@ import {
 import Icone from "../Icone";
 import { useContext } from "react";
 import { CarrinhoContext } from "@/contexts";
+import { limpar as limpaCarrinho } from "@/services/carrinho";
 
 const Carrinho = () => {
 
@@ -66,31 +68,42 @@ const Carrinho = () => {
             }
         }
     }
+
+    const limparCarrinho = () => {
+
+        limpaCarrinho();
+        setItens([]);
+    }
     
     return(
         <div className={ container }>
             <div className={ cabecalho }>
-                <p className={ nomeRestaurante }>Nome do restaurante</p>
-                <p className={ adicionarMaisItens }>Adicionar mais itens</p>
+                <div>
+                    <p className={ nomeRestaurante }>Nome do restaurante</p>
+                    <p className={ adicionarMaisItens }>Adicionar mais itens</p>
+                </div>
+                <p className={ limpar } onClick={ limparCarrinho }>Limpar carrinho</p>
             </div>
             <div className={ pratos }>
             {
-                itens?.map((item) => {
+                
+                    itens?.map((item) => {
 
-                    return(
-                        <div className={ prato } key={ item.id }>
-                            <div className={ info }>
-                                <p className={ nomePrato }>{ item.prato.nome }</p>
-                                <p className={ precoPrato }>{ item.valor }</p>
+                        return(
+                            <div className={ prato } key={ item.id }>
+                                <div className={ info }>
+                                    <p className={ nomePrato }>{ item.prato.nome }</p>
+                                    <p className={ precoPrato }>{ item.valor }</p>
+                                </div>
+                                <div className={ interacoes }>
+                                    <Icone src={ "/icons/add_item_carrinho.svg" } className={ addCarrinho } onClick={ () => incrementarItem(item.id) } />
+                                    <p className={ quantidade }>{ item.quantidade }</p>
+                                    <Icone src={ "/icons/remover_item_carrinho.svg" } className={ removerCarrinho } onClick={ () => decrementarItem(item.id) }/>
+                                </div>
                             </div>
-                            <div className={ interacoes }>
-                                <Icone src={ "/icons/add_item_carrinho.svg" } className={ addCarrinho } onClick={ () => incrementarItem(item.id) } />
-                                <p className={ quantidade }>{ item.quantidade }</p>
-                                <Icone src={ "/icons/remover_item_carrinho.svg" } className={ removerCarrinho } onClick={ () => decrementarItem(item.id) }/>
-                            </div>
-                        </div>
-                    )
-                })
+                        )
+                    })
+                    
             }
             </div>
         </div>
