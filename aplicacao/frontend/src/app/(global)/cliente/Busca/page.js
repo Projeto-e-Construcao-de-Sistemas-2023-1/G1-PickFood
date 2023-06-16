c69575c4d8c44ed92bfb87cdb874f4a9e1c87b6e
jaum1234 <joaovitorsouzacoura@gmail.com>
2023-06-15 21:55:04 -0300

    :bug: fix: corrinndo erro ao entrar em meus pedidos sem nenhum pedido no sistema ainda.|

--- aplicacao/frontend/src/app/(global)/cliente/Busca/page.js
+++ aplicacao/frontend/src/app/(global)/cliente/Busca/page.js
@@ -4,6 +4,11 @@ import ListaRestaurantes from "@/components/ListaRestaurantes";
 import Pesquisa from "@/components/Pesquisa";
 import { buscarRestaurantes } from "@/services/restaurante";
 import { useEffect, useState } from "react";
+import {
+    filtrar,
+    filtros,
+    filtro
+} from "./styles.module.scss";
 
 const Busca = () => {
     const [busca, setBusca] = useState("");
@@ -13,12 +18,31 @@ const Busca = () => {
     useEffect(() => {
         const restaurantesExistente = buscarRestaurantes({nome: busca});
 
+        console.log(busca);
+
         setRestaurantes(restaurantesExistente);
     }, [busca]);
 
     return(
         <>
-            <Pesquisa setBusca={ setBusca } style={{ marginBottom: "20px" }}/>
+            <Pesquisa setBusca={ setBusca }/>
+            <div className={ filtrar }>
+                <p>Filtrar</p>
+            </div>
+            <div className={ filtros }>
+                <div className={ filtro }>
+                    <input type="radio" value={"prato"} name="filtro" onSelect={() => {
+
+                    }}/>
+                    <label>Prato</label>
+                </div>
+                <div>
+                <input type="radio" value={"prato"} name="filtro" onSelect={() => {
+
+                }}/>
+                <label>Restaurante</label>
+                </div>
+            </div>
             <ListaRestaurantes restaurantes={ restaurantes }/>
         </>
     )
