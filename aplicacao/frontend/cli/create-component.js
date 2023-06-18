173894defcd7e09ff33d0d797a2d48c797b07474
jaum1234 <joaovitorsouzacoura@gmail.com>
2023-06-18 10:18:45 -0300

    :sparkles: cli para criar componentes.

---
+++ aplicacao/frontend/cli/create-component.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+const { program } = require("commander");
+const fs = require("fs");
+const path = require("path");
+
+program
+    .command("create-component")
+    .argument("<ComponentName>")
+    .description("Create a simple function component.")
+    .action(componentName => {
+        const dir = path.join(__dirname, "../", "src", "components", componentName);
+
+        fs.mkdirSync(dir, { recursive: true });
+
+        const componentPath = path.join(dir, "index.js");
+        const stylesPath = path.join(dir, "styles.module.scss");
+
+        const componentContent = `import {
+    className
+} from "./styles.module.scss";
+
+const ${componentName} = () => {
+    return(
+        <div className={ className }></div>
+    )
+}
+
+export default ${componentName};
+        `;
+
+        const stylesContent = `.class { /* your style */}`
+
+        fs.writeFileSync(componentPath, componentContent);
+        fs.writeFileSync(stylesPath, stylesContent);
+    });
+
+program.parse(process.argv);
--- aplicacao/frontend/src/components/Logo/index.js
+++ aplicacao/frontend/src/components/Logo/index.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
 
 export default function Logo({ ...restProps }) {
-    return <Image 
+    return <Image
         src="/logo.svg"
-        width={ 57 } 
-        height={ 31 } 
+        width={ 57 }
+        height={ 31 }
         alt="Logo do PickFood. Consiste na palavra 'PickFood' sobrepondo dois circulos: um verde claro e outro verde escuro."
         { ...restProps }
     />
